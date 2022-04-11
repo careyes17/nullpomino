33dd2a0819790d49db74af75472b40253b37eebc
Carlos Reyes <careyes4820@eagle.fgcu.edu>
2022-04-11 06:05:10 -0400

    equivalent method used

--- nullpomino-core/src/main/java/mu/nu/nullpo/game/subsystem/mode/NetDummyMode.java
+++ nullpomino-core/src/main/java/mu/nu/nullpo/game/subsystem/mode/NetDummyMode.java
@@ -1158,7 +1158,7 @@ public class NetDummyMode extends AbstractMode implements NetLobbyListener {
 					String strDate = "----/--/-- --:--:--";
 					Calendar calendar = netRankingDate[d].get(netRankingCursor[d]);
 					if(calendar != null) {
-						strDate = GeneralUtil.getDateAndTime(calendar, TimeZone.getDefault());
+						strDate = GeneralUtil.getDateAndTime(calendar);
 					}
 					receiver.drawMenuFont(engine, playerID, 1, 25, "DATE:" + strDate, EventReceiver.COLOR_CYAN);
 
